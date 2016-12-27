147c795c6d515c9de0d0f1aa2710a18e39d42d90
Jon Pribesh <jpribes@wgu.edu>
2016-12-27 00:51:32 -0500

    Adds start of article widget

--- modules/core/client/directives/article-widget.client.directive.js
+++ modules/core/client/directives/article-widget.client.directive.js
@@ -12,13 +12,24 @@
   function articleWidget($window, ArticlesService) {
     var directive = {
       restrict: 'E',
+      templateUrl: '/modules/core/client/views/article-widget.client.view.html',
       link: link
     };
 
     return directive;
 
     function link(scope, element, attrs) {
-        scope.articles = ArticlesService.query();
+      return ArticlesService.query().$promise.then(function(res) {
+        scope.articles = res;
+        console.log('articles', scope.articles);
+        console.log('articles', scope.articles.length);
+        scope.articles = _.map(scope.articles, function(article) {
+          article.preview = article.content.slice(0, 49);
+          return article;
+        });
+      });
     }
+
+
   }
 }());
